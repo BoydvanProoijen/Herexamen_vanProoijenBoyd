f19ba737ea3d7937cbe0fb8e74ea97a26743c644
BoydvanProoijen <boydvanprooijen@gmail.com>
2018-08-27 20:58:34 +0200

    initial

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -11,19 +11,19 @@ export class RecipeService {
 
   private recipes: Recipe[] = [
     new Recipe(
-      'Tasty Schnitzel',
-      'A super-tasty Schnitzel - just awesome!',
+      'Schnitzel',
+      'Een schnitzel met frietjes!',
       'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
       [
-        new Ingredient('Meat', 1),
-        new Ingredient('French Fries', 20)
+        new Ingredient('Schnitzel', 1),
+        new Ingredient('Frietjes', 20)
       ]),
-    new Recipe('Big Fat Burger',
-      'What else you need to say?',
+    new Recipe('Hamburger',
+      'Een hamburger met vanalles erop en eraan',
       'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
       [
-        new Ingredient('Buns', 2),
-        new Ingredient('Meat', 1)
+        new Ingredient('Brood', 1),
+        new Ingredient('Vlees', 1)
       ])
   ];
 
